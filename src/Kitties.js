462bf77e448dfd472d9b237a355595607060fa2c
Pham Duy Hoan <phamduyhoan95@gmail.com>
2021-12-20 22:36:44 +0700

    [ERROR]Try to do create NFT objects pass parameter to backend

--- src/Kitties.js
+++ src/Kitties.js
@@ -6,6 +6,8 @@ import { TxButton } from './substrate-lib/components';
 
 import KittyCards from './KittyCards';
 
+import {decode as atob, encode as btoa} from 'base-64'
+
 const convertToKittyHash = entry =>
   `0x${entry[0].toJSON().slice(-64)}`;
 
@@ -23,6 +25,7 @@ export default function Kitties (props) {
 
   const [kittyHashes, setKittyHashes] = useState([]);
   const [kitties, setKitties] = useState([]);
+  const [nftObjectBase64Str, setNftObjectBase64Str] = useState([]);
   const [status, setStatus] = useState('');
 
   const subscribeKittyCnt = () => {
@@ -63,6 +66,17 @@ export default function Kitties (props) {
     };
   };
 
+  const onChangeNftObject = async (e) => {
+    console.log('onChangeNftObject');
+    console.log('e inside onChangeNftObject', e);
+    if (e !== undefined) {
+      let nftObjectBase64StrTmp = btoa(e.target.files[0]);
+      setNftObjectBase64Str(nftObjectBase64StrTmp)
+    }
+    // setNftObjectFile(e.target.files[0]);
+    return null;
+  }
+
   useEffect(subscribeKitties, [api, kittyHashes]);
   useEffect(subscribeKittyCnt, [api, keyring]);
 
@@ -70,13 +84,16 @@ export default function Kitties (props) {
   <h1>Kitties</h1>
   <KittyCards kitties={kitties} accountPair={accountPair} setStatus={setStatus}/>
   <Form style={{ margin: '1em 0' }}>
+      <div className="form-group">
+          <input type="file" name="nftObject" onChange={onChangeNftObject}/>
+      </div>      
       <Form.Field style={{ textAlign: 'center' }}>
         <TxButton
           accountPair={accountPair} label='Create Kitty' type='SIGNED-TX' setStatus={setStatus}
           attrs={{
             palletRpc: 'substrateKitties',
             callable: 'createKitty',
-            inputParams: [],
+            inputParams: [nftObjectBase64Str],
             paramFields: []
           }}
         />
